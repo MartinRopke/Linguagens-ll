98086e6c049b4db92de795eed2bad5172549d898
Martin <martin.ropke@gmail.com>
2022-02-19 12:28:43 -0300

    retomar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,9 @@
+var nome;
+var idade = 17
+console.log("Oi, "+ nome)
+if(idade >= 18){
+    var nome = "João"
+    console.log("Parabens, "+ nome)
+    console.log("Agora vc pode dirigir")
+}
+console.log("Até mais "+ nome)
