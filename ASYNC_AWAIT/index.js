8fa88a40f682f89b5f0238f5cee3916869dc7c8e
MartinRopke <martin.ropke@gmail.com>
2022-03-12 12:26:28 -0300

    Creating URL

--- ASYNC_AWAIT/index.js
+++ ASYNC_AWAIT/index.js
@@ -1,1 +1,8 @@
-https://api.openweathermap.org/data/2.5/forecast?q=Itu&appid=
+require('dotenv').config()
+
+//Desestruturar objeto
+
+const { PROTOCOL, BASE_URL, APPID, LANGUAGE, UNITS, Q } = process.env
+const url = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&units=${UNITS}&lang=${LANGUAGE}&q=${Q}`
+
+console.log(url)
