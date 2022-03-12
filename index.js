baabd7c62edfa9ca1e4e283e5b53e0f1e6dea2e4
Martin Ropke <79385261+MartinRopke@users.noreply.github.com>
2022-03-12 10:33:13 -0300

    Initializing JS project

--- index.js
+++
@@ -1,9 +0,0 @@
-var nome;
-var idade = 17
-console.log("Oi, "+ nome)
-if(idade >= 18){
-    var nome = "João"
-    console.log("Parabens, "+ nome)
-    console.log("Agora vc pode dirigir")
-}
-console.log("Até mais "+ nome)
