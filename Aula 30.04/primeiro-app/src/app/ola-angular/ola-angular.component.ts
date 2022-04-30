eb2a278f1c35aee607d82856f199365d6b48ad8e
Martin Ropke <79385261+MartinRopke@users.noreply.github.com>
2022-04-30 12:09:17 -0300

    Aula de Angular

---
+++ Aula 30.04/primeiro-app/src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ola-angular',
+    // template: `
+    // <p>Olá, Angular</p>
+    // `
+    templateUrl: './ola-angular.component.html',
+    styleUrls: ['./ola-angular.component.css']
+})
+export class OlaAngularComponent{
+
+    constructor(){
+
+    }
+
+    ngOnInit(): void {
+        
+    }
+}
