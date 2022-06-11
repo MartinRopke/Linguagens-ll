673cad055c55c9c860c1c8c3ecb1a62928eec5ef
Martin Ropke <79385261+MartinRopke@users.noreply.github.com>
2022-06-11 10:40:38 -0300

    11.06 class

---
+++ Aula 06.04 - 06.11/20221_maua_ecm252_appclientesv2/src/app/clientes/cliente.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from "@angular/core";
+import { Cliente } from "./cliente.model";
+import { Subject } from "rxjs";
+import { map } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class ClienteService {
+  private clientes: Cliente[] = [];
+  private listaClientesAtualizada = new Subject<Cliente[]>();
+
+  constructor(private httpClient: HttpClient){}
+
+  getClientes(): void {
+    this.httpClient.get<{mensagem: string,
+      clientes: any}>('http://localhost:3000/api/clientes')
+      // {_id: 1, nome: Ana, fone: 111, email: ana@email.com} => {id: 1, nome: Ana, fone: 111, email: ana@email.com}
+      .pipe(map((dados) => {
+        return dados.clientes.map((cliente => {
+          return {
+            id: cliente._id,
+            nome: cliente.nome,
+            fone: cliente.fone,
+            email: cliente.email
+          }
+        }))  
+      }))      
+      .subscribe(
+        (clientes) => {
+          this.clientes = clientes;
+          this.listaClientesAtualizada.next([...this.clientes]);
+        }
+      )
+  }
+
+  adicionarCliente(nome: string, fone: string, email:string){
+    const cliente: Cliente = {
+      nome: nome,
+      fone: fone,
+      email: email,
+    };
+    console.log('vai executar o post')
+    this.httpClient.post<{mensagem: string}>('http://localhost:3000/api/clientes',
+    cliente).subscribe(
+      (dados) => {
+        console.log('executou o post')
+        console.log(dados.mensagem);
+        this.clientes.push(cliente);
+        this.listaClientesAtualizada.next([...this.clientes]);
+      }
+    )
+  }
+
+  getListaDeClientesAtualizadaObservable(){
+    return this.listaClientesAtualizada.asObservable();
+  }
+
+  //implementar o método removerCliente
+  //ele recebe o id
+  //faz a requisição http adequada usando o httpClient
+  //exibe uma mensagem confirmando que o cliente de id tal foi removido
+  removerCliente(id: string): void {
+    this.httpClient.delete(`http://localhost:3000/api/clientes/${id}`).subscribe(() => {
+      this.clientes = this.clientes.filter(cli => cli.id !== id)
+      this.listaClientesAtualizada.next([...this.clientes])  
+    })
+  }
+}
